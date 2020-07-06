03bea5527c3d0b3526b690a9c16c86228a897ae3
Jamie <sams_drum@hotmail.com>
2020-07-06 20:35:23 +0100

    .

--- auto-update.sh
+++ auto-update.sh
@@ -1,2 +1,11 @@
-cd /home/pi/tank-pi
-git pull > /dev/null 2>&1
+cd /home/pi/
+
+ORG="JMSams"
+REPO="tank-pi"
+LOCATION=$(curl -s https://api.github.com/repos/$ORG/$REPO/releases/latest \
+| grep "tag_name" \
+| awk '{print "https://github.com/$ORG/$REPO/archive/" substr($2, 2, length($2)-3) ".zip"}')
+curl -L -o temp.zip $LOCATION
+unzip -o -q temp.zip
+
+rm temp.zip
