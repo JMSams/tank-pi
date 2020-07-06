81eeb3c0ee02332e7f93cf7781a14e08dcaee466
Jamie <sams_drum@hotmail.com>
2020-07-06 21:07:06 +0100

    Added .zip to .gitignore, updated auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -1,11 +1,11 @@
 cd /home/pi/
 
-ORG="JMSams"
-REPO="tank-pi"
-LOCATION=$(curl -s https://api.github.com/repos/$ORG/$REPO/releases/latest \
-| grep "tag_name" \
-| awk '{print "https://github.com/$ORG/$REPO/archive/" substr($2, 2, length($2)-3) ".zip"}')
-curl -L -o temp.zip $LOCATION
-unzip -o -q temp.zip
+curl -s https://api.github.com/repos/JMSams/tank-pi/releases/latest \
+| grep "browser_download_url.*tank-pi\.zip" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -q -O temp.zip -i -
+
+unzip -o -q temp.zip -d ./tank-pi
 
 rm temp.zip
